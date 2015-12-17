00a7f4f54d6ccb5caa447ff5cff2bd5a5271d09b
Loretta Cain <locacodes@gmail.com>
2015-12-17 11:34:50 +1100

    Dino skel uploaded

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,16 +1,68 @@
 Map.destroy_all
 
-m1 = Map.create :name => 'Dinobaby', :image => "dinoFull.png"
+m1 = Map.create :name => 'Dino', :image => "dinoskel.png"
 
 Piece.destroy_all
 
-p1 = Piece.create :image => "topRight.png", :x_cor => 176, :y_cor => 86
-p2 = Piece.create :image => "topLeft.png", :x_cor => 103, :y_cor => 103
-p3 = Piece.create :image => "bottomRight.png", :x_cor => 178, :y_cor => 174
-p4 = Piece.create :image => "bottomLeft.png", :x_cor => 30, :y_cor => 174
+p1 = Piece.create :image => "00-00.png", :x_cor => 0.0, :y_cor => 0.0
+p2 = Piece.create :image => "00-50.png", :x_cor => 0.0, :y_cor => 50.00
+p3 = Piece.create :image => "108-407.png", :x_cor => 108.0, :y_cor => 407.0
+p4 = Piece.create :image => "120-324.png", :x_cor => 120.0, :y_cor => 324.0
+p5 = Piece.create :image => "123-443.png", :x_cor => 123.0, :y_cor => 443.0
+p6 = Piece.create :image => "137-336.png", :x_cor => 137.0, :y_cor => 336.0
+p7 = Piece.create :image => "169-481.png", :x_cor => 169.0, :y_cor => 481.0
+p8 = Piece.create :image => "180-40.png", :x_cor => 180.0, :y_cor => 40.0
+p9 = Piece.create :image => "210-60.png", :x_cor => 210.0, :y_cor => 60.0
+p10 = Piece.create :image => "235-218.png", :x_cor => 235.0, :y_cor => 218.0
+p11 = Piece.create :image => "236-86.png", :x_cor => 236.0, :y_cor => 86.0
+p12 = Piece.create :image => "268-92.png", :x_cor => 268.0, :y_cor => 92.0
+p13 = Piece.create :image => "270-95.png", :x_cor => 270.0, :y_cor => 95.0
+p14 = Piece.create :image => "270-468.png", :x_cor => 270.0, :y_cor => 468.0
+p15 = Piece.create :image => "283-328.png", :x_cor => 283.0, :y_cor => 328.0
+p16 = Piece.create :image => "293-200.png", :x_cor => 293.0, :y_cor => 200.0
+p17 = Piece.create :image => "294-337.png", :x_cor => 294.0, :y_cor => 337.0
+p18 = Piece.create :image => "328-226.png", :x_cor => 328.0, :y_cor => 226.0
+p19 = Piece.create :image => "344-178.png", :x_cor => 344.0, :y_cor => 178.0
+p20 = Piece.create :image => "368-147.png", :x_cor => 368.0, :y_cor => 147.0
+p21 = Piece.create :image => "384-170.png", :x_cor => 384.0, :y_cor => 170.0
+p22 = Piece.create :image => "390-70.png", :x_cor => 390.0, :y_cor => 70.0
+p23 = Piece.create :image => "410-76.png", :x_cor => 410.0, :y_cor => 76.0
+p24 = Piece.create :image => "436-130.png", :x_cor => 436.0, :y_cor => 130.0
+p25 = Piece.create :image => "444-80.png", :x_cor => 444.0, :y_cor => 80.0
+p26 = Piece.create :image => "463-86.png", :x_cor => 463.0, :y_cor => 86.0
+p27 = Piece.create :image => "495-88.png", :x_cor => 495.0, :y_cor => 88.0
+p28 = Piece.create :image => "529-471.png", :x_cor => 529.0, :y_cor => 471.0
+p29 = Piece.create :image => "548-95.png", :x_cor => 548.0, :y_cor => 95.0
+p30 = Piece.create :image => "559-294.png", :x_cor => 559.0, :y_cor => 294.0
+p31 = Piece.create :image => "567-100.png", :x_cor => 567.0, :y_cor => 100.0
+p32 = Piece.create :image => "576-303.png", :x_cor => 576.0, :y_cor => 303.0
+p33 = Piece.create :image => "576-507.png", :x_cor => 576.0, :y_cor => 507.0
+p34 = Piece.create :image => "581-162.png", :x_cor => 581.0, :y_cor => 162.0
+p35 = Piece.create :image => "616-111.png", :x_cor => 616.0, :y_cor => 111.0
+p36 = Piece.create :image => "620-430.png", :x_cor => 620.0, :y_cor => 430.0
+p37 = Piece.create :image => "631-332.png", :x_cor => 631.0, :y_cor => 332.0
+p38 = Piece.create :image => "643-352.png", :x_cor => 643.0, :y_cor => 352.0
+p39 = Piece.create :image => "646-191.png", :x_cor => 646.0, :y_cor => 191.0
+p40 = Piece.create :image => "648-116.png", :x_cor => 648.0, :y_cor => 116.0
+p41 = Piece.create :image => "688-136.png", :x_cor => 688.0, :y_cor => 136.0
+p42 = Piece.create :image => "694-468.png", :x_cor => 694.0, :y_cor => 468.0
+p43 = Piece.create :image => "700-100.png", :x_cor => 700.0, :y_cor => 100.0
+p44 = Piece.create :image => "718-198.png", :x_cor => 718.0, :y_cor => 198.0
+p45 = Piece.create :image => "777-134.png", :x_cor => 777.0, :y_cor => 134.0
+p46 = Piece.create :image => "804-146.png", :x_cor => 804.0, :y_cor => 146.0
+p47 = Piece.create :image => "830-284.png", :x_cor => 830.0, :y_cor => 284.0
+p48 = Piece.create :image => "848-168.png", :x_cor => 848.0, :y_cor => 168.0
+p49 = Piece.create :image => "867-268.png", :x_cor => 867.0, :y_cor => 268.0
+p50 = Piece.create :image => "872-199.png", :x_cor => 872.0, :y_cor => 199.0
+p51 = Piece.create :image => "879-238.png", :x_cor => 879.0, :y_cor => 238.0
 
 
-m1.pieces << p1 << p2 << p3 << p4
+
+
+
+
+
+m1.pieces << p1 << p2 << p3 << p4 << p5 << p6 << p7 << p8 << p9 << p10 << p11 << p12 << p13 << p14 << p15 << p16 << p17 << p18 << p19 << p20 << p21 << p22 << p23 << p24 << p25 << p26 << p27 << p28 << p29 << p30 << p31 << p32 << p33 << p34 << p35 << p36 << p37 << p38 << p39 << p40 << p41 << p42 << p43 << p44 << p45 << p46 << p47 << p48 << p49 << p50 << p51
 
 # http://www.shawnimals.com/assets/2061/340x/ThreeLeggedDinoBaby.png?1345600682?1351846921
 
